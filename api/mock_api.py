5d2e43f2cfdb8a0182979058dcb4d6dac6370294
Dharak-Koradia <kdharak99@gmail.com>
2025-05-20 17:39:28 -0700

    Change the way API Server starts and add health endpoint to Server

--- api/mock_api.py
+++ api/mock_api.py
@@ -16,6 +16,10 @@ def login():
         return jsonify({"message": "Login successful", "status": "success"}), 200
     else:
         return jsonify({"message": "Invalid credentials", "status": "fail"}), 401
+    
+@app.route("/health")
+def health_check():
+    return "OK", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=5000)
